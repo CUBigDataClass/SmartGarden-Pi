2d41bad4b6812c160be457e36c789be883d35f4a
Royce Schultz <rosc1351@colorado.edu>
2021-03-15 02:15:43 +0000

    Adds influx db code to send metrics

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 import serial
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
-device = '/dev/cu.usbmodem1411101'
+device = '/dev/ttyUSB0'
+brate = 115200
 brate = 9600
-arduino = serial.Serial(device, brate, timeout=5)
+print('here')
+arduino = serial.Serial(device, brate, timeout=10)
 print(arduino.name)
-for i in range(10):
-    line = arduino.readline()
-    print(line)
+
+# You can generate a Token from the "Tokens Tab" in the UI
+token = "JmcahFWa2q2eRjI1Vmkos51vSeot_IPz1-gZ3a3z2wHu1AaubxYge3it7IjB_Q79D3RY0Ikz4Xe7pCiBu1glig=="
+org = "Falkreath-Hold"
+bucket = "TestBucket"
+
+client = InfluxDBClient(url="http://10.10.8.1:8086", token=token)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+while True:
+    line = arduino.readline().decode('utf-8').strip()
+    data = line.split(':')
+    if len(data) == 2:
+        data = f"{data[0]},host=host1 value={float(data[1])}"
+        write_api.write(bucket, org, data)
+        # time.sleep(0.25)
+        print(f'wrote: {data}')
+    else:
+        print(f'unexpected line format: {line}')
