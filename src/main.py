8f2fbfe314a1ffa1f92524c923883eb5beb9f79b
Royce Schultz <rosc1351@colorado.edu>
2021-03-15 02:45:42 +0000

    Adds config variables to .env file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 import serial
+import os
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-device = '/dev/ttyUSB0'
-brate = 115200
-brate = 9600
-print('here')
+device = os.getenv('DEVICE_PORT', '/dev/ttyUSB0')
+brate = os.getenv('BAUD_RATE', 9600)
+
+print(f'connecting to {device} with {brate}')
+
 arduino = serial.Serial(device, brate, timeout=10)
 print(arduino.name)
 
-# You can generate a Token from the "Tokens Tab" in the UI
-token = "JmcahFWa2q2eRjI1Vmkos51vSeot_IPz1-gZ3a3z2wHu1AaubxYge3it7IjB_Q79D3RY0Ikz4Xe7pCiBu1glig=="
-org = "Falkreath-Hold"
-bucket = "TestBucket"
+influx_db_host = os.environ['DB_HOST']
+token = os.environ['TOKEN']
+org = os.environ['ORG']
+bucket = os.environ['BUCKET']
 
-client = InfluxDBClient(url="http://10.10.8.1:8086", token=token)
+print(f'Connecting to database: {influx_db_host}, bucket: {bucket}')
+
+client = InfluxDBClient(url=influx_db_host, token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 while True:
@@ -23,7 +27,6 @@ while True:
     if len(data) == 2:
         data = f"{data[0]},host=host1 value={float(data[1])}"
         write_api.write(bucket, org, data)
-        # time.sleep(0.25)
         print(f'wrote: {data}')
     else:
         print(f'unexpected line format: {line}')
