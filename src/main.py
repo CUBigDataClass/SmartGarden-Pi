3a496277e6cc53d050d14310d1be184bf4a97f72
Royce Schultz <rosc1351@colorado.edu>
2021-04-17 16:42:03 -0600

    some comments

--- src/main.py
+++ src/main.py
@@ -5,21 +5,21 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 import helpers
 
+# Ardino Setup
 device = os.getenv('DEVICE_PORT', '/dev/ttyUSB0')
 brate = os.getenv('BAUD_RATE', 9600)
 
 print(f'connecting to {device} with {brate}')
-
 arduino = serial.Serial(device, brate, timeout=10)
 print(arduino.name)
 
+# InfluxDB Setup
 influx_db_host = os.environ['DB_HOST']
 token = os.environ['TOKEN']
 org = os.environ['ORG']
 bucket = os.environ['BUCKET']
 
 print(f'Connecting to database: {influx_db_host}, bucket: {bucket}')
-
 client = InfluxDBClient(url=influx_db_host, token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
